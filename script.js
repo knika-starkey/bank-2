2246af12bd628e1c2c5330eec73048e4c4211fe0
knika-starkey <knika787@gmail.com>
2022-11-19 00:32:02 +0200

    links

--- script.js
+++ script.js
@@ -1,42 +1,26 @@
-let card1 = {
-  number: 123456,
-  name: "Maruna",
-  surname: "Ivanova",
-  code: 6785,
-  account: 1000,
-  bank: "Monobank",
-};
-let card2 = {
-  number: 246837,
-  name: "Nikolai",
-  surname: "Lantsov",
-  code: 1213,
-  account: 357010,
-  bank: "Monobank",
-};
-let card3 = {
-  number: 246837,
-  name: "Nicolai",
-  surname: "Lantsov",
-  code: 1214,
-  account: 357010,
-  bank: "Monobank",
-};
+function Card(number, name, surname, code, account, bank) {
+  this.number = number;
+  this.name = name;
+  this.surname = surname;
+  this.code = code;
+  this.account = account;
+  this.bank = bank;
+}
 
-let atm1 = {
-  cards: [card1, card2],
-  showAccount: function (card) {
+function Atm(cards) {
+  this.cards = cards;
+  this.showAccount = function (card) {
     if (this.compare(card)) alert(card.account);
     else alert("Error");
-  },
-  getMoney: function (card, sum) {
+  };
+  this.getMoney = function (card, sum) {
     if (isNaN(sum)) alert("It's not a number");
     else if (this.compare(card)) {
       if (sum > card.account) alert("You have less money!");
       else card.account -= sum;
     } else alert("Wrong card");
-  },
-  compare: function (card) {
+  };
+  this.compare = function (card) {
     for (let i = 0; i < this.cards.length; i++) {
       if (
         this.cards[i].code == card.code &&
@@ -45,8 +29,15 @@ let atm1 = {
         return true;
     }
     return false;
-  },
-};
+  };
+}
+
+let card1 = new Card(123456, "Maruna", "Ivanova", 6785, 1000, "Monobank");
+let card2 = new Card(246837, "Nikolai", "Lantsov", 1213, 357010, "Monobank");
+let card3 = new Card(246837, "Vasiliy", "Lantsov", 1214, 357010, "Monobank");
+
+let atm1 = new Atm([card1, card2]);
+
 alert(atm1.compare(card1));
 atm1.showAccount(card1);
 atm1.getMoney(card1, 30);
